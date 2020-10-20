f3e2df283cc0ed6c7f074afc3a769e6a00bc7b95
meachamm92 <meacham.space@gmail.com>
2020-10-19 23:26:42 -0500

    fixed solutions again

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,7 +33,7 @@ console.log(power(2, 5));
 function factorial(a) {
   let sum = a;
   while (a > 1) {
-    sum = multiply(sum, a - 1);
+    sum = multiply(sum, add(a, -1));
     a = add(a, -1);
   }
   return sum;
@@ -43,12 +43,20 @@ console.log(factorial(5));
 
 // Kata 5:
 function fibonacci(n) {
-  let numbers = [0];
+  let numbers = [];
+  numbers.push(0);
   if (n >= 2) {
     numbers.push(1);
-    for (i = 2; i < n; add(i, 1)) {
-      numbers[i] = add(numbers[i - 2], numbers[i - 1]);
+    let count = n;
+    let i = 2;
+    while (count > 0) {
+      numbers[i] = add(numbers[add(i, -2)], numbers[add(i, -1)]);
+      i = add(i, 1);
+      count = add(count, -1);
     }
+    // for (i = 2; i < n; i++) {
+    //   numbers[i] = add(numbers[add(i, -2)], numbers[add(i, -1)]);
+    // }
   }
   return numbers[add(n, -1)];
 }
