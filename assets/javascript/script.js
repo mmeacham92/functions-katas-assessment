4875a3f64f777f8b937f1b7af3a21e6a29eff0bf
meachamm92 <meacham.space@gmail.com>
2020-10-19 22:53:20 -0500

    fixed solutions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,57 +3,54 @@ function add(a, b) {
   return a + b;
 }
 
-console.log(`The sum is ${add(3, 5)}`);
+console.log(`The sum is ${add(44, 5)}`);
 
 // Kata 2:
 function multiply(a, b) {
   let product = 0;
   while (b > 0) {
     product = add(product, a);
-    b--;
+    b = add(b, -1);
   }
   return product;
 }
 
-console.log(`The product is ${multiply(12, 5)}`);
+console.log(`The product is ${multiply(12, 10)}`);
 
 // Kata 3:
 function power(x, n) {
   let product = x;
   while (n > 1) {
     product = multiply(product, x);
-    n--;
+    n = add(n, -1);
   }
   return product;
 }
 
-let total = power(5, 7);
-console.log(total);
+console.log(power(2, 5));
 
 // Kata 4:
 function factorial(a) {
   let sum = a;
   while (a > 1) {
     sum = multiply(sum, a - 1);
-    a--;
+    a = add(a, -1);
   }
   return sum;
 }
 
-console.log(factorial(6));
+console.log(factorial(5));
 
 // Kata 5:
 function fibonacci(n) {
-  let numbers = [];
-  let sum = 0;
-  numbers.push(sum);
+  let numbers = [0];
   if (n >= 2) {
-      numbers.push(1);
-      for (i = 2; i < n; i++) {
-          numbers[i] = add(numbers[i-2], numbers[i-1]);
-      }
+    numbers.push(1);
+    for (i = 2; i < n; add(i, 1)) {
+      numbers[i] = add(numbers[i - 2], numbers[i - 1]);
+    }
   }
-  return numbers[n - 1];
+  return numbers[add(n, -1)];
 }
 
-console.log(fibonacci(33));
+console.log(fibonacci(8));
